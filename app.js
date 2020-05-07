6c894c452862321b5c6b24ec34f567d0b740b920
Jollyrgr83 <jasongmangin@gmail.com>
2020-05-07 16:40:43 -0600

    exported questions to separated js file

--- app.js
+++ app.js
@@ -9,51 +9,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-// stores questions for use in inquirer prompts
-const questions = {
-    type: {
-        type: "checkbox",
-        name: "type",
-        message: "Which type of employee would you like to add?",
-        choices: ["manager", "engineer", "intern"]
-    },
-    name: {
-        type: "input",
-        name: "name",
-        message: "What is the employee's name?"
-    },
-    id : {
-        type: "input",
-        name: "id",
-        message: "What is the employee's id?"
-    },
-    email: {
-        type: "input",
-        name: "email",
-        message: "What is the employee's email address?"
-    },
-    manager: {
-        type: "input",
-        name: "extra",
-        message: "What is the employee's office number?"
-    },
-    engineer: {
-        type: "input",
-        name: "extra",
-        message: "What is the employee's github username?"
-    },
-    intern: {
-        type: "input",
-        name: "extra",
-        message: "What school is the employee attending?"
-    },
-    continue: {
-        type: "checkbox",
-        name: "continue",
-        message: "Would you like to add another employee?",
-        choices: ["Yes", "No"]
-    }
-}
+
+const questions = require("./lib/questions");
+
 // initializes employee array for use in rendering html
 const employeeArray = [];
 // contains user prompts to construct employee objects and push them to employee array
